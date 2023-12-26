a8e21e1a0cfbb294a6fc573db97f3d517aaed05b
tawfeek shalash <t.shalash1@gmail.com>
2023-12-26 20:58:41 +0200

    return exercise8 and exercise9

--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -12,14 +12,19 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
+//        ReactiveSources.intNumbersFluxWithException()
+//                .doOnError(err -> System.out.println("Error " + err.getMessage()))
+//                .subscribe(e -> System.out.println(e));
 
         // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
-
+//        ReactiveSources.intNumbersFluxWithException()
+//                .onErrorContinue((err, item) -> System.out.println("Error " + err.getMessage()))
+//                .subscribe(e -> System.out.println(e));
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorResume(err -> Flux.just(-1, -2))
+                .subscribe(e -> System.out.println(e));
 
         System.out.println("Press a key to end");
         System.in.read();
--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -9,14 +9,21 @@ public class Exercise9 {
 
         // Use ReactiveSources.intNumbersFlux()
 
-        // Print size of intNumbersFlux after the last item returns
-        // TODO: Write code here
+//        // Print size of intNumbersFlux after the last item returns
+//        ReactiveSources.intNumbersFlux()
+//                .count()
+//                .subscribe(e -> System.out.println(e));
 
-        // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
-
+//        // Collect all items of intNumbersFlux into a single list and print it
+//        ReactiveSources.intNumbersFlux()
+//                .collectList()
+//                .subscribe(e -> System.out.println(e));
+//
         // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux()
+                .buffer(2)
+                .map(list -> list.get(0) + list.get(1))
+                .subscribe(e -> System.out.println(e));
 
         System.out.println("Press a key to end");
         System.in.read();
